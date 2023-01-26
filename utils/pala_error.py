53db2c769388343652e4714903b0489f287d440e
hahnec <christopher.hahne@unibe.ch>
2023-01-26 22:11:10 +0100

    refactor(l2): rename error

--- utils/pala_error.py
+++ utils/pala_error.py
@@ -10,21 +10,21 @@ def rmse_unique(pt_array, gt_array, tol=1/4):
     mask = np.ones(len(pt_array), dtype=bool)
     fn_num = 0
 
-    errs = []
+    l2_errs = []
     for gt in gt_array:
-        ssd = ((pt_array-gt)**2).sum(-1)**.5
-        err, idx = np.min(ssd), np.argmin(ssd)
-        if err < tol:# and mask[idx]:
-            errs.append(err)
+        l2_distances = ((pt_array-gt)**2).sum(-1)**.5
+        l2_err, idx = np.min(l2_distances), np.argmin(l2_distances)
+        if l2_err < tol:# and mask[idx]:
+            l2_errs.append(l2_err)
             mask[idx] = False
         else:
             fn_num += 1
     
     fp_num = sum(mask)
-    tp_num = len(errs)
+    tp_num = len(l2_errs)
 
     jaccard = jaccard_index(tp_num, fn_num, fp_num) * 100
     precision = tp_num/(fp_num+tp_num) * 100 if fp_num+tp_num != 0 else 0
     recall = tp_num/(fn_num+tp_num) * 100 if fn_num+tp_num != 0 else 0
 
-    return np.mean(errs), precision, recall, jaccard, tp_num, fp_num, fn_num
+    return np.mean(l2_errs), precision, recall, jaccard, tp_num, fp_num, fn_num
