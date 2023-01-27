813992befa2320bd8e0a139858b38ded24c1cd32
hahnec <christopher.hahne@unibe.ch>
2023-01-27 01:33:59 +0100

    feat(cmap): add color map and normalization capability

--- utils/render_ulm.py
+++ utils/render_ulm.py
@@ -6,6 +6,9 @@ from simple_tracker.tracking2d import tracking2d
 from simple_tracker.tracks2img import tracks2img
 
 
+normalize = lambda x: (x-x.min())/(x.max()-x.min()) if x.max()-x.min() > 0 else x-x.min()
+
+
 def render_ulm(data_path=None, method='default', expr='', plot_opt=False):
 
     # path management
@@ -42,15 +45,37 @@ def render_ulm(data_path=None, method='default', expr='', plot_opt=False):
         shifted_coords = [np.hstack([p[:, :2] - origin[:2], p[:, 2:]]) for p in tracks_out]
         ulm_img, vel_map = tracks2img(shifted_coords, img_size=np.array([84, 134]), scale=10, mode='tracks')#velnorm')
 
+    # normalize images
+    ulm_img = normalize(ulm_img)
+    vel_map = normalize(vel_map)
+
+    ulm_img = img_color_map(img=ulm_img, cmap='gnuplot')
+    vel_map = img_color_map(img=vel_map, cmap='plasma')
+
     if plot_opt:
         plt.figure()
         plt.imshow(ulm_img, cmap='gnuplot')#'inferno'
         plt.show()
 
+        plt.figure()
         plt.imshow(vel_map, cmap='plasma')#'inferno'
         plt.show()
-    
+
+    # convert to uint8
+    ulm_img = np.array(normalize(ulm_img) * (2**8-1), dtype='uint8')
+    vel_map = np.array(normalize(vel_map) * (2**8-1), dtype='uint8')
+
     return ulm_img, vel_map
+
+def img_color_map(img=None, cmap='inferno'):
+
+    # get color map
+    colormap = plt.get_cmap(cmap)
+
+    # apply color map omitting alpha channel
+    img = colormap(img)[..., :3]
+
+    return img
 
 
 if __name__ == '__main__':
