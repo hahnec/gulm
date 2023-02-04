dddeb847428d38a6730949f02e64f4cc641af211
Hahne Christopher (ch21z139) <ch21z139@submit01.ubelix.unibe.ch>
2023-02-04 10:52:30 +0100

    fix(arr): rectify array handling

--- bash_scripts/infer_pulm_array.sh
+++ bash_scripts/infer_pulm_array.sh
@@ -17,11 +17,9 @@ module load Python/3.8.6-GCCcore-10.2.0
 
 source ~/21_rethink_ulm/venv/bin/activate
 
-cd ~/21_rethink_ulm/scripts
-
 python -c "import torch; print(torch.cuda.is_available())"
 
-param_store=param.txt
+param_store=./bash_scripts/array_params.txt
 
 ch_gap=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $1}')
 enlarge_factor=$(cat $param_store | awk -v var=$SLURM_ARRAY_TASK_ID 'NR==var {print $2}')
@@ -33,4 +31,4 @@ cd ~/21_rethink_ulm/scripts/
 python -c "import torch; print(torch.cuda.is_available())"
 
 echo "Channel gap: ${ch_gap}, Upsampling factor: ${enlarge_factor}, Cluster number:${cluster_number}, Max-iter: ${max_iter}"
-python ./pala_memgo+ellipse_gte+localize-2ch_script_dev_frame_batch.py ch_gap=${ch_gap} enlarge_factor=${enlarge_factor} cluster_number=${cluster_number} max_iter=${max_iter} dat_num=2
+python ./pala_memgo+ellipse_gte+localize-2ch_script_dev_frame_batch.py ch_gap=${ch_gap} enlarge_factor=${enlarge_factor} cluster_number=${cluster_number} max_iter=${max_iter} dat_num=16
