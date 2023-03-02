81c8ec27a69111b5c865d0b81e50cbe92dd7255a
Hahne Christopher (ch21z139) <ch21z139@submit01.ubelix.unibe.ch>
2023-03-02 18:01:59 +0100

    feat(write): prevent overriding different noise results

--- scripts/pala_memgo+ellipse_gte+localize-2ch_script_dev_frame_batch.py
+++ scripts/pala_memgo+ellipse_gte+localize-2ch_script_dev_frame_batch.py
@@ -111,7 +111,7 @@ if cfg.logging:
     wandb.define_metric('PALA/FalsePositive', step_metric='frame')
     wandb.define_metric('PALA/FalseNegative', step_metric='frame')
 
-output_path = script_path / 'other_frames'
+output_path = script_path / 'other_frames_'+str(cfg.clutter_db)
 if cfg.save_opt and not output_path.exists(): output_path.mkdir()
 
 if cfg.plt_comp_opt or cfg.plt_cluster_opt:
