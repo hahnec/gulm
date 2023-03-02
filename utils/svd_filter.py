c8d3b724d6080bceb6ccc137c664bd39efcb81ce
hahnec <christopher.hahne@unibe.ch>
2023-03-02 16:50:44 +0100

    feat(invivo): add invivo script and utilities

---
+++ utils/svd_filter.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def svd_filter(iq):
+
+    init_shape = iq.shape
+
+    # reshape into Casorati matrix
+    x = iq.reshape(-1, iq.shape[-1], order='F')
+
+    # calculate svd of the autocorrelated Matrix
+    u, _, _ = np.linalg.svd(x.T@x)
+
+    # calculate the singular vectors
+    v = x@u
+
+    # singular value decomposition
+    n = v@u.T
+
+    # Reconstruction of the final filtered matrix
+    iqf = n.reshape(init_shape, order='F')   
+
+    return iqf
