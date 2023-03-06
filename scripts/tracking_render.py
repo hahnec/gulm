eacf0599a85bf7260b99eed4b997187e2a202ccf
hahnec <christopher.hahne@unibe.ch>
2023-03-06 20:40:28 +0100

    fix(tracks): rectify dimensions for missing points in frames

--- scripts/tracking_render.py
+++ scripts/tracking_render.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('../')
 
+import numpy as np
 from pathlib import Path
 from omegaconf import OmegaConf
 
@@ -14,14 +15,19 @@ cfg = OmegaConf.load(str(script_path.parent / 'config_invivo.yaml'))
 # override config with CLI
 cfg = OmegaConf.merge(cfg, OmegaConf.from_cli())
 
-run_name = 'twilight-universe-545'    #'bright-grass-569'   #
+run_name = 'kind-gorge-589'    #'bright-grass-569'   #
 output_path = Path(cfg.data_dir) / 'Results' / ('invivo_frames_'+run_name)
 
 frames = load_ulm_data(data_path=str(output_path), expr='pace')
 
-ulm_img_wo_tracks, _ = render_ulm(frames, tracking=None, plot_opt=False, cmap_opt=True, uint8_opt=False, gamma=cfg.gamma, srgb_opt=True, wavelength=9.856e-05)
+frame_end_idx = -1
 
-ulm_img_wi_tracks, _ = render_ulm(frames, tracking='hungarian', plot_opt=False, cmap_opt=True, uint8_opt=False, gamma=cfg.gamma, srgb_opt=True, wavelength=9.856e-05)
+size = None     #np.array([78,  118], dtype=int)
+origin = None   #np.array([-59,  0, 0], dtype=int)
+
+ulm_img_wo_tracks, _ = render_ulm(frames[:frame_end_idx], tracking=None, plot_opt=False, cmap_opt=True, uint8_opt=False, gamma=cfg.gamma, srgb_opt=True, wavelength=9.856e-05, size=size, origin=origin)
+
+ulm_img_wi_tracks, _ = render_ulm(frames[:frame_end_idx], tracking='hungarian', plot_opt=False, cmap_opt=True, uint8_opt=False, gamma=cfg.gamma, srgb_opt=True, wavelength=9.856e-05, size=size, origin=origin, fps=1000)
 
 import wandb
 wandb.init(project="pulm_renderer", name=run_name, config=cfg, group=None)
--- utils/render_ulm.py
+++ utils/render_ulm.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../')
+
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -10,11 +13,12 @@ from utils.srgb_conv import srgb_conv
 normalize = lambda x: (x-x.min())/(x.max()-x.min()) if x.max()-x.min() > 0 else x-x.min()
 
 
-def render_ulm(frames, tracking=None, expr='', plot_opt=False, cmap_opt=False, uint8_opt=False, gamma=None, srgb_opt=False, wavelength=None, origin=None):
+def render_ulm(frames, tracking=None, expr='', plot_opt=False, cmap_opt=False, uint8_opt=False, gamma=None, srgb_opt=False, wavelength=None, size=None, origin=None, fps = 500):
 
     # init variables
     wavelength = 9.856e-05 if wavelength is None else wavelength
     origin = np.array([-72,  16, 0], dtype=int) if origin is None else origin
+    size = np.array([84, 134]) if size is None else size
 
     # remove empty arrays
     frames = [f for f in frames if f.size > 0]
@@ -24,13 +28,12 @@ def render_ulm(frames, tracking=None, expr='', plot_opt=False, cmap_opt=False, u
         min_len = 15
         max_linking_distance = 2
         max_gap_closing = 0
-        framerate = 500
 
         # render based on Hungarian linker
         frames = [f / wavelength for f in frames]
-        tracks_out, tracks_interp = tracking2d(frames, max_linking_distance=max_linking_distance, max_gap_closing=max_gap_closing, min_len=min_len, scale=1/framerate, mode='interp')
+        tracks_out, tracks_interp = tracking2d(frames, max_linking_distance=max_linking_distance, max_gap_closing=max_gap_closing, min_len=min_len, scale=1/fps, mode='interp')
         shifted_coords = [np.hstack([p[:, :2] - origin[:2], p[:, 2:]]) for p in tracks_out]
-        ulm_img, vel_map = tracks2img(shifted_coords, img_size=np.array([84, 134]), scale=10, mode='tracks')#velnorm')
+        ulm_img, vel_map = tracks2img(shifted_coords, img_size=size, scale=10, mode='tracks')#velnorm')
     else:
         # render based on localizations
         all_pts = np.vstack(frames) / wavelength - origin[:2]
@@ -76,7 +79,7 @@ def img_color_map(img=None, cmap='inferno'):
 def load_ulm_data(data_path, expr='pace'):
 
     # path management
-    script_path = Path(__file__).parent.parent.resolve() / 'scripts' / 'other_frames'
+    script_path = Path(__file__).resolve().parent.parent / 'scripts' / 'other_frames'
     data_path = data_path if data_path is not None else script_path
     fnames = sorted(Path(data_path).iterdir())
     assert len(fnames) > 0, 'No files found'
@@ -87,7 +90,8 @@ def load_ulm_data(data_path, expr='pace'):
         # skip files which do not contain expression
         if fname.name.__contains__(expr):
             arr = np.loadtxt(fname, delimiter=',', skiprows=1)
-            if arr.size == 0: arr = np.array([[],[]]).T     # change dimensions of empty array
+            if arr.size == 0: arr = np.ones([1, 2])*float('nan')     # change dimensions of empty array
+            if len(arr.shape) != 2: arr = arr[None, :]
             frames.append(arr)
     assert len(frames) > 0, 'No frames found'
 
@@ -97,5 +101,6 @@ def load_ulm_data(data_path, expr='pace'):
 if __name__ == '__main__':
 
     data_path = None
-    frames = load_ulm_data(data_path, expr='pace')
-    render_ulm(frames, plot_opt=True, gamma=.5, srgb_opt=True)
+    frames = load_ulm_data(data_path, expr='pala')
+    render_ulm(frames, plot_opt=True, gamma=.9, srgb_opt=True)
+    render_ulm(frames, tracking='hungarian', plot_opt=True, gamma=.9, srgb_opt=True)
